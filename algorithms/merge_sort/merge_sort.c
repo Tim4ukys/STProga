fb25e8650fc6747d8a042d0bff088fb1b7505951
Tim4ukys <t.kireev@g.nsu.ru>
2025-09-28 16:57:16 +0700

    add merge sort

---
+++ algorithms/merge_sort/merge_sort.c
@@ -0,0 +1,51 @@
+﻿#include "merge_sort.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <memory.h>
+#include <stdbool.h>
+
+#define get(p, i, sz) (uint8_t*)(p)+(i)*(sz)
+
+void merge_sort(void* arr, size_t lenght, size_t element_size, merge_cmp_func cmp_func)
+{
+	if (lenght <= 1 || !cmp_func || !arr || !element_size)
+		return;
+
+	for (size_t pow = 2; pow / 2 < lenght; pow *= 2) {
+		void* pTemp = malloc(pow * element_size);
+		assert(pTemp && "couldn't allocate memory");
+		
+		// block - индекс в массиве начала блока размера pow
+		for (size_t block = 0; block < lenght; block += pow) {
+			// если другая половина блока выходит за размеры массива, то очевидно
+			// что этот блок отсортирован был ранее
+			if (block + pow / 2 >= lenght)
+				break;
+
+			for (size_t a = 0, b = pow / 2, i = 0; i < pow;) {
+				void* pA = get(arr, block + a, element_size);
+				void* pB = get(arr, block + b, element_size);
+				if (b < pow && cmp_func(pA, pB) > 0) {
+					memcpy(get(pTemp, i++, element_size), pB, element_size);
+					b++;
+					if (b + block == lenght || b == pow) {
+						memcpy(get(pTemp, i, element_size), pA, element_size * (pow / 2 - a));
+						break;
+					}
+				}
+				else if (a < pow / 2) {
+					memcpy(get(pTemp, i++, element_size), pA, element_size);
+					a++;
+					if (a == pow / 2) {
+						memcpy(get(pTemp, i, element_size), pB, element_size * (pow - b));
+						break;
+					}
+				}
+			}
+			
+			memcpy(get(arr, block, element_size), pTemp, element_size * (block + pow > lenght ? lenght - block : pow));
+		}
+		
+		free(pTemp);
+	}
+}
---
+++ tests/algorithms/test_merge_sort.cpp
@@ -0,0 +1,49 @@
+﻿#include <gtest/gtest.h>
+#include <Windows.h>
+#include <ctime>
+
+extern "C" {
+#include <merge_sort.h>
+}
+
+static int __cdecl cmp_int(const void* a, const void* b) {
+	return *(int*)b - *(int*)a;
+}
+
+static int g_arrNumbs[]{ -11, 2022, 02, 24, 228 };
+static int g_arrNumbsOrig[]{ -11, 2022, 02, 24, 228 };
+static int g_arrNumbsSorted[]{ 2022, 228, 24, 2, -11 };
+
+TEST(Algorithms_Merge_Sort, OnlyNull) {
+	merge_sort(nullptr, 0, 0, nullptr);
+
+	merge_sort(g_arrNumbs, ARRAYSIZE(g_arrNumbs), 0, cmp_int);
+	merge_sort(g_arrNumbs, 0, sizeof(int), cmp_int);
+	merge_sort(g_arrNumbs, 1, sizeof(int), cmp_int);
+	merge_sort(g_arrNumbs, ARRAYSIZE(g_arrNumbs), sizeof(int), nullptr);
+	EXPECT_FALSE(memcmp(g_arrNumbs, g_arrNumbsOrig, sizeof(g_arrNumbs)));
+}
+
+TEST(Algorithms_Merge_Sort, Baza) {
+	merge_sort(g_arrNumbs, ARRAYSIZE(g_arrNumbs), sizeof(int), cmp_int);
+	EXPECT_FALSE(memcmp(g_arrNumbs, g_arrNumbsSorted, sizeof(g_arrNumbs)));
+}
+
+TEST(Algorithms_Merge_Sort, RandomNumbs) {
+	constexpr size_t NUMBS_COUND = 10'000u;
+	srand(time(NULL));
+	auto numbs_1 = (int*)malloc(NUMBS_COUND * sizeof(int));
+	auto numbs_2 = (int*)malloc(NUMBS_COUND * sizeof(int));
+	assert(numbs_1 && numbs_2 && "couldn't allocate memory");
+
+	for (size_t i = 0; i < NUMBS_COUND; i++) {
+		numbs_1[i] = numbs_2[i] = rand() % 200'000 - 100'000;
+	}
+
+	merge_sort(numbs_1, NUMBS_COUND, sizeof(*numbs_1), cmp_int);
+	qsort(numbs_2, NUMBS_COUND, sizeof(*numbs_1), cmp_int);
+	EXPECT_FALSE(memcmp(numbs_1, numbs_2, NUMBS_COUND * sizeof(int)));
+
+	free(numbs_1);
+	free(numbs_2);
+}
