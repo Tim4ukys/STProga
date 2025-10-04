477d2a4326c785501425a7cc485f9c4aa48c0625
Tim4ukys <t.kireev@g.nsu.ru>
2025-10-05 02:05:41 +0700

    add KMP algorithms

---
+++ algorithms/kmp_search/kmp_search.c
@@ -0,0 +1,51 @@
+﻿#include "kmp_search.h"
+#include <inttypes.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void kmp_init_lps(const char* pattern, size_t* lps) {
+    if (!pattern || !*pattern || !lps)
+        return;
+
+    lps[0] = 0;
+
+    for (size_t x = 0, y = 1; pattern[y];) {
+        if (pattern[x] == pattern[y]) {
+            lps[y++] = ++x;
+        } else if (x) {
+            x = lps[x - 1];
+        } else {
+            lps[y++] = 0;
+        }
+    }
+}
+
+const char* kmp_search(const char* str, const char* pattern) {
+    if (!str || !pattern || !*str || !*pattern)
+        return (char*)-1;
+
+    const size_t szPattern = strlen(pattern);
+    if (strlen(str) < szPattern)
+        return (char*)-1;
+
+    size_t* lps = malloc(sizeof(size_t) * szPattern);
+    assert(lps && "couldn't allocate memory");
+    kmp_init_lps(pattern, lps);
+
+    for (size_t x = 0, y = 0; str[x];) {
+        if (str[x] == pattern[y]) {
+            ++x;
+            if (++y >= szPattern) {
+                free(lps);
+                return str + x - szPattern;
+            }
+        } else if (y) {
+            y = lps[y - 1];
+        } else {
+            ++x;
+        }
+    }
+    free(lps);
+    return (char*)-1;
+}
---
+++ tests/algorithms/test_kmp_search.cpp
@@ -0,0 +1,42 @@
+﻿#include <gtest/gtest.h>
+#include <utility>
+#include <string_view>
+#include <map>
+
+extern "C" {
+#include <kmp_search.h>
+}
+
+extern "C" void kmp_init_lps(const char*, size_t*);
+
+TEST(Algorithms_kmp_search, TestLps) {
+    std::pair<std::string_view, std::vector<size_t>> numbs[]{
+        {"ababcabab", {0, 0, 1, 2, 0, 1, 2, 3, 4}},
+        {"abcdabca", {0, 0, 0, 0, 1, 2, 3, 1}},
+        {"aabaaac", {0, 1, 0, 1, 2, 2, 0}},
+        {"aabaabaaaabaabаaab", {0, 1, 0, 1, 2, 3, 4, 5, 2, 2, 3, 4, 5, 6, 7, 8, 9, 3}}};
+
+    for (auto& [pattern, ref_lps] : numbs) {
+        std::vector<size_t> lps;
+        lps.resize(ref_lps.size());
+
+        kmp_init_lps(const_cast<char*>(pattern.data()), lps.data());
+        EXPECT_FALSE(memcmp(lps.data(), ref_lps.data(), lps.size()));
+    }
+}
+
+TEST(Algorithms_kmp_search, FindNeedle) {
+    std::map<std::string_view, std::vector<std::string_view>> tests{
+        {"abcbecbabcbc", {"cbc", "abcbc", "e", "bab", "cbe", "abcbecbabcbc", "abcbecbabcbcdgdfhdgf", "test"}},
+        {"aboba", {"ba", "bob", "oba", "o", "a", "test"}}};
+
+    for (auto& [haystack, needle] : tests) {
+        for (auto& i : needle) {
+            auto        f = haystack.find(i);
+            const char* p = kmp_search(haystack.data(), i.data());
+
+            EXPECT_TRUE((f == std::string_view::npos && p == (void*)-1) ||
+                        (p != (void*)-1 && f != std::string_view::npos) && (haystack.data() + f == p));
+        }
+    }
+}
